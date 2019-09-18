0982bce591c64e8e84d8a4ee6271d737051bb4b3
parky7 <parkyandrew1@gmail.com>
2019-09-18 14:03:16 -0700

    Started 3 buttons

--- app/src/main/java/com/example/soundboard/SoundBoardActivity.java
+++ app/src/main/java/com/example/soundboard/SoundBoardActivity.java
@@ -2,13 +2,104 @@ package com.example.soundboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SoundBoardActivity extends AppCompatActivity {
+
+    private Button buttonA;
+    private Button buttonBb;
+    private Button buttonB;
+    private SoundPool soundPool;
+    private boolean isSoundPoolLoaded;
+    private int aNote;
+    private int bbNote;
+    private int bNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        wireWidgets();
+        intializeSoundPool();
+
+
+        setListeners();
+    }
+
+    private void intializeSoundPool() {
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        aNote = soundPool.load(this, R.raw.scalea, 1);
+        bbNote = soundPool.load(this, R.raw.scalebb, 1);
+        bNote = soundPool.load(this, R.raw.scaleb, 1);
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+    }
+
+
+    private void wireWidgets() {
+        buttonA = findViewById(R.id.button_main_A);
+        buttonB = findViewById(R.id.button_main_b);
+        buttonBb = findViewById(R.id.button_main_bb);
+    }
+
+    private void setListeners() {
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                // Is the sound loaded already?
+                if (isSoundPoolLoaded) {
+                    soundPool.play(aNote, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                }
+
+            }
+        });
+        buttonBb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                // Is the sound loaded already?
+                if (isSoundPoolLoaded) {
+                    soundPool.play(bbNote, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                }
+
+            }
+        });
+        buttonB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                float actualVolume = (float) audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                float maxVolume = (float) audioManager
+                        .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                float volume = actualVolume / maxVolume;
+                // Is the sound loaded already?
+                if (isSoundPoolLoaded) {
+                    soundPool.play(bNote, volume, volume, 1, 0, 1f);
+                    Log.e("Test", "Played sound");
+                }
+
+            }
+        });
     }
 }
